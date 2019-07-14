abae2c30ed6292b38c9158e8ff83d75c5fa7633a
kig6911 <brittani.kiger@gmail.com>
2019-07-14 01:38:16 -0400

    Created simple program and finished it

--- simple/src/simple/simple.java
+++ simple/src/simple/simple.java
@@ -1,5 +1,10 @@
 package simple;
 
-public class simple {
+/*This is a simple program */
 
+class simple {
+	//A java program begins with a call to main()
+	public static void main(String args[]) {
+		System.out.println("Java drives the web.");
+	}
 }
